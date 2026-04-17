22b272478de8c7b183f7de6b32ef695d56a5d8c6
Mekan Sari <mekanxsari@gmail,com>
2026-04-17 21:28:51 +0300

    добавление UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Modal from './components/Modal';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isModalOpen, setIsmodalOpen] = useState(false);
+    const [selectedProductId, setSelectedProductId] = useState(null);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    const productClick = (productId) => {
+        setSelectedProductId(productId);
+        setIsmodalOpen(true);
+    };
 
-      <div className="ticks"></div>
+    return (
+        <>
+            <div className="search-wrapper">
+                <input type="search" placeholder='Поиск товаров...' />
+                <select name='categories'>
+                    <option value="">Все категории</option>
+                    <option value="electronics">Электроника</option>
+                    <option value="clothing">Одежда</option>
+                    <option value="books">Книги</option>
+                </select>
+            </div>
+            <div className='product-wrapper'>
+                <ul className='product-list'>
+                    <li>
+                        <a href='#' data-id='1' onClick={ (e) => { e.preventDefault(); productClick(1) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='2' onClick={ (e) => { e.preventDefault(); productClick(2) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='3' onClick={ (e) => { e.preventDefault(); productClick(3) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='4' onClick={ (e) => { e.preventDefault(); productClick(4) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='5' onClick={ (e) => { e.preventDefault(); productClick(5) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='6' onClick={ (e) => { e.preventDefault(); productClick(6) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='7' onClick={ (e) => { e.preventDefault(); productClick(7) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='8' onClick={ (e) => { e.preventDefault(); productClick(8) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='9' onClick={ (e) => { e.preventDefault(); productClick(9) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='#' data-id='10' onClick={ (e) => { e.preventDefault(); productClick(10) } }>
+                            <img
+                                src='https://imgcdn.befree.ru/rest/V1/images/1280/product/images/BF2623120043/BF2623120043_4_1.webp'
+                                className='product-image'
+                                alt='Image Description'
+                            />
+                            <div className='product-details'>
+                                <p className='product-name'>Футболка хлопковая рваная с принтом</p>
+                                <span className='product-price'>499 ₽</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+            <Modal 
+                isOpen={isModalOpen} 
+                onClose={() => setIsmodalOpen(false)}
+                productId={selectedProductId}
+            />
+        </>
+    )
 }
 
 export default App
